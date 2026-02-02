abe0b6ede33dfc266bdfba4849c314139854927b
J SHAKTHI SRAVANTH <shakthisravanth001@gmail.com>
2026-02-02 16:23:49 +0530

    lab - 8 : Initial commit

--- EmployeeApi/src/main/java/com/service/EmployeeService.java
+++ EmployeeApi/src/main/java/com/service/EmployeeService.java
@@ -7,10 +7,13 @@ import org.springframework.stereotype.Service;
 import com.model.Employee;
 import com.repository.EmployeeRepository;
 import com.exception.EmployeeNotFoundException;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class EmployeeService {
+
+    private static final Logger log = LoggerFactory.getLogger(EmployeeService.class);
 
     private final EmployeeRepository employeeRepository;
 
@@ -19,6 +22,7 @@ public class EmployeeService {
     }
 
     public Employee createEmployee(Employee employee) {
+        log.info("Creating employee: " + employee.getName());
         return employeeRepository.save(employee);
     }
 
@@ -29,8 +33,10 @@ public class EmployeeService {
     public Employee getEmployeeById(Integer id) {
 
         if (employeeRepository.findById(id).isPresent()) {
+            log.info("Fetching employee with id: " + id);
             return employeeRepository.findById(id).get();
         } else {
+            log.error("Employee not found with id: " + id);
             throw new EmployeeNotFoundException("Employee not found with id: " + id);
         }
     }
@@ -43,9 +49,12 @@ public class EmployeeService {
     public void deleteEmployee(Integer id) {
 
 		if (employeeRepository.existsById(id)) {
-				employeeRepository.deleteById(id);
+            log.info("Deleting employee with id: " + id);
+
+			employeeRepository.deleteById(id);
 		} else {
-				throw new EmployeeNotFoundException("Employee not found with id: " + id);
+            log.error("Delete failed. Employee not found: " + id);
+			throw new EmployeeNotFoundException("Employee not found with id: " + id);
 		}
     }
 }
